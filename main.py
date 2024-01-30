a297a7c15ce0bdce18ef5c029d126713e8be3aaa
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-01-30 22:45:01 +0300

    Latest

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
+# #!/usr/bin/env python
+# # coding: utf-8
 
-from blogic import * 
+# from blogic import * 
 
-if __name__ == "__main__":   
-    # TEST
+# if __name__ == "__main__":   
+#     # TEST
