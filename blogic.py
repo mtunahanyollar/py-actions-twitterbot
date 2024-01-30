e3c6db1117b083705348682bff387509597a67fd
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-01-31 00:56:35 +0300

    Latest

--- blogic.py
+++ blogic.py
@@ -25,6 +25,43 @@ def gasoline(city):
     #print("Kursunsuz_95(Excellium95)_TL/lt:", values[0])
     #print("Motorin(Eurodiesel)_TL/lt:", values[1])
     return values
+def currencies():
+    import requests
+    import json
+    baseUrl = "https://hasanadiguzel.com.tr/api/kurgetir"
+    response = requests.get(baseUrl)
+    data = ""
+    euro_values = {
+        "BanknoteBuying": "",
+        "BanknoteSelling": ""
+    }
+
+    usd_values = {
+        "BanknoteBuying": "",
+        "BanknoteSelling": ""
+    }
+    if response.status_code == 200:
+    # Print the response content (the data returned by the API)
+        #print(response.json())  # Assuming the response is in JSON format
+        json_string = json.dumps(response.json(), indent=2)
+        data = json.loads(json_string)
+        for currency in data["TCMB_AnlikKurBilgileri"]:
+            if currency["Isim"] == "EURO":
+                euro_values["BanknoteBuying"] = str(currency["BanknoteBuying"])
+                euro_values["BanknoteSelling"] = str(currency["BanknoteSelling"])
+            elif currency["Isim"] == "ABD DOLARI":
+                usd_values["BanknoteBuying"] = str(currency["BanknoteBuying"])
+                usd_values["BanknoteSelling"] = str(currency["BanknoteSelling"])
+
+
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+        print(response.text)  # Print the response content in case of an error
+
+    # Print the extracted values
+    #print("Kursunsuz_95(Excellium95)_TL/lt:", values[0])
+    #print("Motorin(Eurodiesel)_TL/lt:", values[1])
+    return euro_values, usd_values
 # Auth
 tt = tweepy.Client(
     #Consumer Keys
@@ -36,8 +73,10 @@ tt = tweepy.Client(
   
 # ## Defining Functions
     
-values = gasoline("ISTANBUL")
-finalText = "Kagithane Akaryakit Fiyatlari: \n"+ "Kursunsuz_95(Excellium95)_TL/lt " + values[0] + "\n" + "Motorin(Eurodiesel)_TL/lt " + values[1]
+gasolineValues = gasoline("ISTANBUL")
+euro_values, usd_values = currencies()
+finalText = "L o L :  \n"+ "Kursunsuz_95(Excellium95)_TL/lt " + gasolineValues[0] + "\n" + "Motorin(Eurodiesel)_TL/lt " + gasolineValues[1]
+finalText = finalText + "\n\n" + "USD ALIŞ: " + usd_values["BanknoteBuying"] + "\nUSD SATIŞ: " + usd_values["BanknoteSelling"]+ "\n\nEURO ALIŞ: " + euro_values["BanknoteBuying"] + "\nEURO SATIŞ: " +  euro_values["BanknoteSelling"]
 print(finalText)
 tt.create_tweet(text= finalText)
 
