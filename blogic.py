2145d33ec4b4da6255685937e13f32afd1a9354b
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-01-30 20:39:10 +0300

    first commit

---
+++ blogic.py
@@ -0,0 +1,14 @@
+import os
+import requests
+import tweepy
+from datetime import datetime as dt
+
+# ## Auth
+
+tt = tweepy.Client(
+    #Consumer Keys
+    consumer_key= os.environ['CONSUMER_KEY'],
+    consumer_secret= os.environ['CONSUMER_SECRET'],
+    # Access Token and Secret
+    access_token= os.environ['ACCESS_TOKEN'],
+    access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
