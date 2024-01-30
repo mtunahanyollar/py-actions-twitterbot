ae9cbd47148feca5218354cf150e79735726d5bd
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-01-30 21:10:29 +0300

    firstss

--- blogic.py
+++ blogic.py
@@ -14,8 +14,8 @@ from datetime import datetime as dt
 
 tt = tweepy.Client(
     #Consumer Keys
-    consumer_key= os.environ['CONSUMER_KEY'],
-    consumer_secret= os.environ['CONSUMER_SECRET'],
+    consumer_key= os.environ['CONSUMER_TOKEN'],
+    consumer_secret= os.environ['CONSUMER_TOKEN_SECRET'],
     # Access Token and Secret
     access_token= os.environ['ACCESS_TOKEN'],
     access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
