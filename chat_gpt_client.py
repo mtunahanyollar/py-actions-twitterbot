29e1193c605760becf37efe119d5e9a2aa29853f
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-02-13 23:32:39 +0300

    Chatgpt Client & Tw client added

---
+++ chat_gpt_client.py
@@ -0,0 +1,54 @@
+import os
+import openai
+
+class ChatGptClient:
+    def __init__(self):
+        # Retrieve the API key from the environment variable
+        # api_key = os.environ.get("OPENAI_API_KEY")
+        api_key = "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"
+        if not api_key:
+            raise ValueError("OPENAI_API_KEY environment variable is not set.")
+        
+        # Initialize the OpenAI API client with the retrieved API key
+        openai.api_key = api_key
+
+    def generate_response(self, user_message):
+        # Generate a response from the ChatGPT model given a user message
+        completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {
+                "role": "user",
+                "content": user_message,
+            },
+        ],
+        )
+        return completion.choices[0].message.content
+
+        # for chunk in stream:
+        #     if chunk.choices[0].delta.content is not None:
+        #         print(chunk.choices[0].delta.content, end="")
+        #         return chunk.choices[0].delta.content  # Return the generated response text
+
+
+# from openai import OpenAI
+
+# client = OpenAI(
+#   api_key="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"
+# )
+
+# input_text = "Cok yaratici bir tweetter kullanicisisin. komik ve ayni zamanda motivasyon odakli tweetler hazirliyorsun. Tweetlerini okuyanlar derhal ise koyulmayi dusunuyorlar, bir ornek verir misin?"
+# # Make the API call
+# client.Com
+# stream = client.chat.completions.create(
+#     model="gpt-3.5-turbo-0125",
+#     messages=[{"role": "user", "content": input_text}],
+#     stream=True,
+# )
+# for chunk in stream:
+#     if chunk.choices[0].delta.content is not None:
+#         print(chunk.choices[0].delta.content, end="")
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-# #!/usr/bin/env python
-# # coding: utf-8
+from chat_gpt_client import ChatGptClient
+from twitter_client import TwitterClient
 
-# from blogic import * 
+def main():
+    # Initialize ChatGptClient
+    chat_gpt_client = ChatGptClient()
 
-# if __name__ == "__main__":   
-#     post_date()
+    # Generate response using ChatGptClient
+    user_message = "Turk sairlerden guzel emek siirleri bul ve getir. Yazari en sona ekleyerek yaz bana"
+    response = chat_gpt_client.generate_response(user_message)
+    print("ChatGPT Response:", response)
+
+    # Initialize TwitterClient
+    twitter_client = TwitterClient()
+
+    # Get the Twitter Client
+    twitter_api_client = twitter_client.get_client()
+    print("Twitter Client:", twitter_api_client)
+
+    twitter_api_client.create_tweet(text = response)
+
+if __name__ == "__main__":
+    main()
