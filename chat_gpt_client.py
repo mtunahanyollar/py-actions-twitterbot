4795f9e52e42775a4873515a93ec551c7e9ea69a
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-02-13 23:47:23 +0300

    G e n e r a l

--- chat_gpt_client.py
+++ chat_gpt_client.py
@@ -1,6 +1,7 @@
 import os
 import openai
-
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 class ChatGptClient:
     def __init__(self):
         # Retrieve the API key from the environment variable
