361df348507576ff204e111fc2407966810ed589
Tunahan Yollar <mtunahanyollar@gmail.com>
2024-02-14 00:26:56 +0300

    Prompts with Random added

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import random
 from chat_gpt_client import ChatGptClient
 from twitter_client import TwitterClient
 
 def main():
     # Initialize ChatGptClient
     chat_gpt_client = ChatGptClient()
-
+    topics = ["Bana bir film onerisinde bulun ve en fazla 30 kelime bu filmi ozetle", "Bana bir Serdar Ortac sarkisindan dortluk ver", "Guzel bir ingilizce kelime ve turkce anlamiyla birlikte paylas. en fazla 30 kelime.", "Turk bir sairden alinti yap; dortlukten ileriye gitmesin"]
+    result = random.choice(topics)
+    print(result)
     # Generate response using ChatGptClient
-    user_message = "Bana bir film onerisinde bulun ve en fazla 30 kelime bu filmi ozetle"
+    user_message = result
     response = chat_gpt_client.generate_response(user_message)
     print("ChatGPT Response:", response)
 
